c4e507ffe6fe82a741a4e80a449c042872544f58
Reisy <reisy1b@gmail.com>
2025-07-20 10:52:32 +0300

    finish mageing

--- packages/backend/src/config/dbConnection.ts
+++ packages/backend/src/config/dbConnection.ts
@@ -1,18 +1,35 @@
+// import { Pool } from 'pg';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+// console.log('Database user:', process.env.PGUSER);
+// console.log('Database host:', process.env.PGHOST);
+// console.log('Database name:', process.env.PGDATABASE);
+// console.log('Database password:', process.env.PGPASSWORD);
+// console.log('Database port:', process.env.PGPORT);
+// const pool = new Pool({
+
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: Number(process.env.PGPORT),
+//  });
+// console.log('Database connection pool created');
+// export {pool}
+
+
+
 import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-console.log('Database user:', process.env.DB_USER);
-console.log('Database host:', process.env.DB_HOST);
-console.log('Database name:', process.env.DB_NAME);
-console.log('Database password:', process.env.DB_PASSWORD);
-console.log('Type:', typeof process.env.DB_PASSWORD);
+
 
 console.log('Database port:', process.env.DB_PORT);
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: Number(process.env.PGPORT),
  });
@@ -20,3 +37,7 @@ console.log('Database connection pool created');
 export {pool}
 
 
+
+export const pool = new Pool({
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
