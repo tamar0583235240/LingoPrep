d7b6df3ea5c5ff29d5f6f385950a8f646ff5bac9
BatShevagit <sh0548503408@gmail.com>
2025-07-30 19:01:24 +0300

    enter

--- packages/backend/src/config/dbConnection.ts
+++ packages/backend/src/config/dbConnection.ts
@@ -1,7 +1,13 @@
-// import { Pool } from 'pg';
-// import dotenv from 'dotenv';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
+console.log('Database user:', process.env.DB_NAME);
+console.log('Database host:', process.env.DB_HOST);
+console.log('Database name:', process.env.DB_NAME);
+console.log('Database password:', process.env.DB_PASSWORD);
+console.log('Database port:', process.env.DB_PORT);
+
 // console.log('Database user:', process.env.PGUSER);
 // console.log('Database host:', process.env.PGHOST);
 // console.log('Database name:', process.env.PGDATABASE);
@@ -15,22 +21,34 @@
 //   password: process.env.PGPASSWORD,
 //   port: Number(process.env.PGPORT),
 //  });
-// console.log('Database connection pool created');
-// export {pool}
-
-
-
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-import { createClient } from '@supabase/supabase-js';
-dotenv.config();
-
-
-export const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT),
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
- });
+ const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 console.log('Database connection pool created');
+export {pool}
+
+
+
+// import { Pool } from 'pg';
+// import dotenv from 'dotenv';
+// import { createClient } from '@supabase/supabase-js';
+// dotenv.config();
+
+
+// export const pool = new Pool({
+//   host: process.env.PGHOST,
+//   port: Number(process.env.PGPORT),
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+//  });
+// console.log('Database connection pool created');
+
+
